90527f9a81af03945dd45c0380837d4c089d50cf
Kit Burgess <kitburgess>
2021-01-05 11:51:05 +0000

    chore: Store this here for now, TODO

--- vortexasdk/config.py
+++ vortexasdk/config.py
@@ -1,4 +1,18 @@
+"""module level Scooby dooby"""
 import os
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING").upper()
+
+class Config:
+    """Class level dooby"""
+
+    @property
+    def LOG_LEVEL(self):
+        """Log level property"""
+        return os.getenv("LOG_LEVEL", "WARNING").upper()
+
+
+LOG_LEVEL = "WARNING"
 LOG_FILE = os.getenv("LOG_FILE", None)
+
+HTTP_PROXY = os.getenv("HTTP_PROXY", None)
+HTTPS_PROXY = os.getenv("HTTPS_PROXY", None)
